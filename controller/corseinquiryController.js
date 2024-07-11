c17c5e08beb2b23f3ee84e71c9b739563fe10e0b
krunal-29 <krunalmistry7545@gmail.com>
2024-07-11 13:04:34 +0530

    kalp

--- controller/corseinquiryController.js
+++ controller/corseinquiryController.js
@@ -1,4 +1,4 @@
-const {CourseInquirymodel} = require('../model/corseinquiryshcema')
+const { CourseInquirymodel } = require('../model/corseinquiryshcema')
 
 const addInquiry = (req, res) => {
     let { FullName,
@@ -8,18 +8,19 @@ const addInquiry = (req, res) => {
         Description,
         CollageName,
         Course,
-        
+
         Interaction,
         FollowUp } = req.body
-        let Testarr=Course&&Course.map((ele)=>{
-            return{Course:ele,
-                isAdded:false
-            }
-        })
-        console.log(Testarr)
+    let Testarr = Course && Course.map((ele) => {
+        return {
+            Course: ele,
+            isAdded: false
+        }
+    })
+    console.log(Testarr)
 
     const data = new CourseInquirymodel({
-        
+
         FullName,
         Contact,
         Email,
@@ -34,26 +35,26 @@ const addInquiry = (req, res) => {
         Course,
         Interaction,
         FollowUp,
-        isAdded:false
+        isAdded: false
     })
 
     // let {error, value}=validation.validate({})
     // if(error){
-        // res.send({error})
+    // res.send({error})
     // }else{
 
-        data.save().then((data1) => {
-            res.send({ mag: "Inquiry Added", data1 })
+    data.save().then((data1) => {
+        res.send({ mag: "Inquiry Added", data1 })
+    })
+        .catch((err) => {
+            res.send({ err })
         })
-            .catch((err) => {
-                res.send({ err })
-            })
-    }
+}
 // }
 
 const updateinquiry = (req, res) => {
     let { FullName,
-        
+
         Contact,
         Email,
         Date,
@@ -64,11 +65,12 @@ const updateinquiry = (req, res) => {
         Interaction,
         FollowUp } = req.body
 
-        let Testarr=Course&&Course.map((ele)=>{
-            return{Course:ele,
-                isAdded:false
-            }
-        })
+    let Testarr = Course && Course.map((ele) => {
+        return {
+            Course: ele,
+            isAdded: false
+        }
+    })
 
     // let {error, value}=validation.validate({FullName,
     //     Contact,
@@ -84,13 +86,13 @@ const updateinquiry = (req, res) => {
     //     res.send({error})
     // }else{
 
-    CourseInquirymodel.updateOne({ _id: req.query.id }, {...req.body,Testarr})
-            .then((data) => {
-                res.send({ msg: "Inquiry Updated", data })
-            })
-            .catch((err) => {
-                res.send({ err })
-            })
+    CourseInquirymodel.updateOne({ _id: req.query.id }, { ...req.body, Testarr })
+        .then((data) => {
+            res.send({ msg: "Inquiry Updated", data })
+        })
+        .catch((err) => {
+            res.send({ err })
+        })
     // }
 }
 
@@ -98,7 +100,7 @@ const updateinquiry = (req, res) => {
 const deletinquiry = (req, res) => {
     CourseInquirymodel.find({ _id: req.query.id }).then((data) => {
         let obj = JSON.parse(JSON.stringify(data))
-        obj.isDeleted=true
+        obj.isDeleted = true
 
         CourseInquirymodel.updateOne({ _id: req.query.id }, obj).then((dataup) => {
             res.send({ msg: "Inquiry Soft Deleted" })
@@ -110,9 +112,9 @@ const deletinquiry = (req, res) => {
         .catch((err) => {
             res.send({ err })
         })
-    
+
 }
-const hardelet=(req,res)=>{
+const hardelet = (req, res) => {
 
     CourseInquirymodel.deleteOne({ _id: req.query.id })
         .then((data) => {
@@ -132,7 +134,7 @@ const displayInquiry = (req, res) => {
 }
 
 const displayOnGoingInquiry = (req, res) => {
-    CourseInquirymodel.find({ onGoing: true,isDeleted:false }).then((data) => {
+    CourseInquirymodel.find({ onGoing: true, isDeleted: false }).then((data) => {
         res.send({ msg: "display Inquiry", data })
     })
         .catch((err) => {
@@ -141,7 +143,7 @@ const displayOnGoingInquiry = (req, res) => {
 }
 
 const displayRejectInquiry = (req, res) => {
-    CourseInquirymodel.find({ Reject: true,isDeleted:false  }).then((data) => {
+    CourseInquirymodel.find({ Reject: true, isDeleted: false }).then((data) => {
         res.send({ msg: "display Inquiry", data })
     })
         .catch((err) => {
@@ -151,7 +153,7 @@ const displayRejectInquiry = (req, res) => {
 
 
 const displayConfirmInquiry = (req, res) => {
-    CourseInquirymodel.find({ Confirm: true ,isDeleted:false }).then((data) => {
+    CourseInquirymodel.find({ Confirm: true, isDeleted: false }).then((data) => {
         res.send({ msg: "display Inquiry", data })
     })
         .catch((err) => {
@@ -209,94 +211,92 @@ const getISAddeddata = (req, res) => {
             }
         }
     })
-    .then((data) => {
-        res.send({ msg: "allIsAdded", data });
-    })
-    .catch((err) => {
-        res.send({ err });
-    });
+        .then((data) => {
+            res.send({ msg: "allIsAdded", data });
+        })
+        .catch((err) => {
+            res.send({ err });
+        });
 };
 
 const fillterbyDate = (req, res) => {
     let key = req.query.key
     let sortby = req.query.sortby
-    
 
-   
-    
-        CourseInquirymodel.find().sort({ [key]: parseInt(sortby) })
-            .then((data) => {
-                res.send({ data, msg: "else fillter" })
-            })
-            .catch((err) => {
-                res.send({ err })
-            })
 
-    
+
+
+    CourseInquirymodel.find().sort({ [key]: parseInt(sortby) })
+        .then((data) => {
+            res.send({ data, msg: "else fillter" })
+        })
+        .catch((err) => {
+            res.send({ err })
+        })
+
+
 }
 
 
 const filterByMonth = async (req, res) => {
-    let {  month, sort } = req.query
-    
+    let { month, sort } = req.query
 
-    
-        try {
-            const januaryData = await CourseInquirymodel.find({
-                $expr: {
-                    $eq: [{ $month: "$Date" }, month]
-                }
-            }).sort({Date:sort});
-            res.json(januaryData);
-        } catch (err) {
-            res.status(500).json({ error: err.message });
-        }
-    
+
+
+    try {
+        const januaryData = await CourseInquirymodel.find({
+            $expr: {
+                $eq: [{ $month: "$Date" }, month]
+            }
+        }).sort({ Date: sort });
+        res.json(januaryData);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+
 };
 
 
 
-const confirmsearch=async(req,res)=>{
-    let {FullName} = req.query
-    const populatedata =await CourseInquirymodel.find({Confirm: true ,isDeleted:false})
+const confirmsearch = async (req, res) => {
+    let { FullName } = req.query
+    const populatedata = await CourseInquirymodel.find({ Confirm: true, isDeleted: false })
 
- if (populatedata.length>0) {
+
+        const filterdata = populatedata.filter((ele) => {
+            return ele.FullName && ele.FullName.toLowerCase() == FullName.toLowerCase()
+        })
+        res.send({ filterdata })
     
-     const filterdata= populatedata.filter((ele)=>{
-       return ele.FullName.toLowerCase() == FullName.toLowerCase()
-      })
-      res.send({filterdata})
- }
- }
+}
 
 
- const onGoingsearch=async(req,res)=>{
-    let {FullName} = req.query
+const onGoingsearch = async (req, res) => {
+    let { FullName } = req.query
+
+    const populatedata = await CourseInquirymodel.find({ onGoing: true, isDeleted: false })
+  
+
+        const filterdata = populatedata.filter((ele) => {
+            return ele.FullName && ele.FullName.toLowerCase() == FullName.toLowerCase()
+        })
+        res.send({ filterdata })
     
-    const populatedata =await CourseInquirymodel.find({onGoing: true,isDeleted:false})
-    if (populatedata.length>0) {
-        
-        const filterdata= populatedata.filter((ele)=>{
-          return ele.FullName.toLowerCase() == FullName.toLowerCase()
-         })
-         res.send({filterdata})
-    }
- }
+}
+
+const rejectsearch = async (req, res) => {
+    let { FullName } = req.query
+    const populatedata = await CourseInquirymodel.find({ Reject: true, isDeleted: false })
 
 
- const rejectsearch=async(req,res)=>{
-    let {FullName} = req.query
-    const populatedata =await CourseInquirymodel.find({Reject: true,isDeleted:false })
- if(populatedata.length>0){
-
-     const filterdata= populatedata.filter((ele)=>{
-       return ele.FullName.toLowerCase() == FullName.toLowerCase()
-      })
-      res.send({filterdata})
- }
- }
+        const filterdata = populatedata.filter((ele) => {
+            return ele.FullName && ele.FullName.toLowerCase() == FullName.toLowerCase() 
+        })
+        res.send({ filterdata })
+    
+}
 
 
 
 
-module.exports = { addInquiry, updateinquiry, deletinquiry,rejectsearch,confirmsearch,onGoingsearch, displayOnGoingInquiry, displayInquiry, displayRejectInquiry, displayConfirmInquiry, RejectInquiry, ConfirmInquiry,getISAddeddata ,fillterbyDate,filterByMonth}
+module.exports = { addInquiry, updateinquiry, deletinquiry, rejectsearch, confirmsearch, onGoingsearch, displayOnGoingInquiry, displayInquiry, displayRejectInquiry, displayConfirmInquiry, RejectInquiry, ConfirmInquiry, getISAddeddata, fillterbyDate, filterByMonth }
