eca35a18663b3d072fcfa80f060b6b14faea87c7
krunal-29 <krunalmistry7545@gmail.com>
2024-07-16 12:25:29 +0530

    dghj

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ app.use(function (req, res, next) {
 app.use(express.json())
 
 
-app.use(bodyParser.json({ limit: '1mb' }));
-app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ limit: '1mb', extended: true }));
 
 app.use('/login',login)
 app.use('/invoice', rout)
