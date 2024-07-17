54f3a4d5564b687d74912c66df91b2ae9edfb464
krunal-29 <krunalmistry7545@gmail.com>
2024-07-17 12:15:08 +0530

    gfhj

--- controller/studentController.js
+++ controller/studentController.js
@@ -1,174 +1,133 @@
-const { stuModel } = require('../model/studentShcema')
-const { AddCourseModel} = require('../model/addCorsebatch')
+const { stuModel } = require('../model/studentShcema');
+const { AddCourseModel } = require('../model/addCorsebatch');
 
-const addStudent = async(req, res) => {
-    let { Name,
-        CourseId,
-        Contact,
-        Email,
-        CollegeName,
-        AcademicCourse,
-        course,
-        Date,
-        baseString,
-        btime,
-        Parentcontact,
-        Tfees,
-        
-        
+const addStudent = async (req, res) => {
+  try {
+    let { 
+      Name, CourseId, Contact, Email, CollegeName, AcademicCourse, course, Date, 
+      baseString, btime, Parentcontact, Tfees 
+    } = req.body;
 
-
-    } = req.body
-    
     const stuData = new stuModel({
-        Name,
-        Contact,
-        CourseId,
-        Email,
-        CollegeName,
-        AcademicCourse,
-        Rfees:parseInt(Tfees),
-        course,
-        Date,
-        btime,
-        Parentcontact,
-        
-        Pfees:parseInt(0),
-        Tfees:parseInt(Tfees),
-        baseString,
-        
-    })
-
-    stuData.save().then((data) => {
-        res.send({ msg: "Student Data Added", data })
-    })
-        .catch((err) => {
-            res.send({ err })
-        })
-
-}
-
-const updateStu = (req, res) => {
-
-   
-    stuModel.updateOne({ _id: req.query.id }, {...req.body,CourseId:req.body.CourseId._id})
-        .then((data) => {
-            res.send({ msg: "Student Updated", data })
-        })
-        .catch((err) => {
-            res.send({ err })
-        })
-
-}
-
-const deleteStu = (req, res) => {
-    stuModel.deleteOne({ _id: req.query.id })
-        .then((data) => {
-            res.send({ msg: "Student DEleted" })
-        })
-        .catch((err) => {
-            res.send({ err })
-        })
-}
-
-const getAllStu = (req, res) => {
-    stuModel.find({ CourseId: req.query.id }).then((data) => {
-        res.send({ msg: "All student", data })
-    })
-        .catch((err) => {
-            res.send({ err })
-        })
-}
-const Alldata = (req, res) => {
-    stuModel.find().populate("CourseId").then((data) => {
-        res.send({ data })
-    }).catch((err) => {
-        res.send({ err })
-    })
-}
-const InvoiceGet = (req, res) => {
-    // Extract the course ID from the request query parameters
-    const courseId = req.query.id;
-
-    // Define the query to find students with the given course ID and Rfess not equal to 0
-    stuModel.find({ CourseId: courseId, Rfess: { $ne: 0 } }, (err, students) => {
-        if (err) {
-            // Handle error
-            return res.status(500).send({ error: 'Database query failed' });
-        }
-        // Send the retrieved students as the response
-        res.status(200).send(students);
+      Name, Contact, CourseId, Email, CollegeName, AcademicCourse, 
+      Rfees: parseInt(Tfees), course, Date, btime, Parentcontact, 
+      Pfees: 0, Tfees: parseInt(Tfees), baseString
     });
+
+    const data = await stuData.save();
+    res.status(201).json({ msg: "Student Data Added", data });
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).json({ err });
+  }
 };
 
-const fillterbyDate = (req, res) => {
-    let key = req.query.key
-    let sortby = req.query.sortby
-    let courseid = req.query.courseid
+const updateStu = async (req, res) => {
+  try {
+    const data = await stuModel.updateOne({ _id: req.query.id }, { ...req.body, CourseId: req.body.CourseId._id });
+    res.status(200).json({ msg: "Student Updated", data });
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).json({ err });
+  }
+};
 
+const deleteStu = async (req, res) => {
+  try {
+    await stuModel.deleteOne({ _id: req.query.id });
+    res.status(200).json({ msg: "Student Deleted" });
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).json({ err });
+  }
+};
+
+const getAllStu = async (req, res) => {
+  try {
+    const data = await stuModel.find({ CourseId: req.query.id });
+    res.status(200).json({ msg: "All student", data });
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).json({ err });
+  }
+};
+
+const Alldata = async (req, res) => {
+  try {
+    const data = await stuModel.find().populate("CourseId");
+    res.status(200).json({ data });
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).json({ err });
+  }
+};
+
+const InvoiceGet = async (req, res) => {
+  try {
+    const courseId = req.query.id;
+    const students = await stuModel.find({ CourseId: courseId, Rfees: { $ne: 0 } });
+    res.status(200).json(students);
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).json({ error: 'Database query failed' });
+  }
+};
+
+const fillterbyDate = async (req, res) => {
+  try {
+    let { key, sortby, courseid } = req.query;
+    sortby = parseInt(sortby);
+
+    let data;
     if (!courseid) {
-
-        stuModel.find().sort({ [key]: parseInt(sortby) })
-            .then((data) => {
-                res.send({ data, msg: " is fillter" })
-            })
-            .catch((err) => {
-                res.send({ err })
-            })
+      data = await stuModel.find().sort({ [key]: sortby });
     } else {
-        stuModel.find({ CourseId: courseid }).sort({ [key]: parseInt(sortby) })
-            .then((data) => {
-                res.send({ data, msg: "else fillter" })
-            })
-            .catch((err) => {
-                res.send({ err })
-            })
-
+      data = await stuModel.find({ CourseId: courseid }).sort({ [key]: sortby });
     }
-}
 
+    res.status(200).json({ data, msg: "Filtered" });
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).json({ err });
+  }
+};
 
 const filterByMonth = async (req, res) => {
-    let { perentId, month, sort } = req.query
-    sort=parseInt(sort)
+  try {
+    let { perentId, month, sort } = req.query;
+    sort = parseInt(sort);
+
+    let data;
     if (!perentId) {
-
-        try {
-            const januaryData = await stuModel.find({
-                $expr: {
-                    $eq: [{ $month: "$Date" }, month]
-                }
-            }).sort({Date:sort});
-            res.json(januaryData);
-        } catch (err) {
-            res.status(500).json({ error: err.message });
+      data = await stuModel.find({
+        $expr: {
+          $eq: [{ $month: "$Date" }, month]
         }
+      }).sort({ Date: sort });
+    } else {
+      data = await stuModel.find({
+        $expr: {
+          $eq: [{ $month: "$Date" }, month]
+        },
+        CourseId: perentId
+      }).sort({ Date: sort });
     }
 
-    else {
-        try {
-            const januaryData = await stuModel.find({
-                $expr: {
-                    $eq: [{ $month: "$Date" }, month]
-                }
-                , CourseId: perentId
-            }).sort({Date:sort});
-            res.json(januaryData);
-        } catch (err) {
-            res.status(500).json({ error: err.message });
-        }
-    }
+    res.status(200).json(data);
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).json({ error: err.message });
+  }
 };
 
-const search=(req,res)=>{
-    
-     stuModel.find({Name:req.query.Name}).then((data)=>{
+const search = async (req, res) => {
+  try {
+    const data = await stuModel.find({ Name: req.query.Name });
+    res.status(200).json({ data });
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).json({ err });
+  }
+};
 
-         res.send({data})
-     })
-     .catch((err)=>{
-        res.send({err})
-     })
- 
- }
-module.exports = { addStudent, updateStu,search, deleteStu, getAllStu, fillterbyDate, Alldata, filterByMonth }
+module.exports = { addStudent, updateStu, search, deleteStu, getAllStu, fillterbyDate, Alldata, filterByMonth, InvoiceGet };
