6f63876634a2d29dfe38c86625c6b0b7872e8551
krunal-29 <krunalmistry7545@gmail.com>
2024-07-19 18:34:16 +0530

    ttyui

--- controller/IsCompletedController.js
+++ controller/IsCompletedController.js
@@ -1,27 +1,47 @@
 const { BatchCompletedModel } = require("../model/BatchCompleted");
 
-const Update=async(req,res)=>{
+const Update = async (req, res) => {
+  try {
+    let UpdateId = req.query.UpdateId;
+    let { Studentid, Date, Cetificate } = req.body;
+    console.log(req.body, req.query);
 
-    let UpdateId=req.query.UpdateId;
-    let {Studentid,Date,Cetificate}=req.body
-console.log(req.body,req.query)
-BatchCompletedModel.findOne({_id:req.query.UpdateId}).then(async(copy)=>{
-        console.log(copy)
-    let cp=JSON.parse(JSON.stringify(copy))
-    let index=cp.StudentArray.findIndex((ele)=>(ele._id==Studentid))
-    cp.StudentArray[index].Date=Date
-    cp.StudentArray[index].Cetificate=Cetificate
+    let copy = await BatchCompletedModel.findOne({ _id: UpdateId });
+    if (!copy) {
+      return res.status(404).send({ msg: "Batch not found" });
+    }
 
+    console.log(copy);
+    let cp = JSON.parse(JSON.stringify(copy));
+    let index = cp.StudentArray.findIndex((ele) => ele._id == Studentid);
+    if (index === -1) {
+      return res.status(404).send({ msg: "Student not found" });
+    }
 
+    cp.StudentArray[index].Date = Date;
+    cp.StudentArray[index].Cetificate = Cetificate;
 
-   const updateData=await  BatchCompletedModel.updateOne({_id:UpdateId},cp)
-})
-   res.send({msg:"data updated"})
+    const updateData = await BatchCompletedModel.updateOne({ _id: UpdateId }, cp);
+
+    if (updateData.nModified === 0) {
+      return res.status(400).send({ msg: "No changes made" });
+    }
+
+    res.send({ msg: "Data updated successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ msg: "Internal server error", error: error.message });
+  }
 }
 
-const getAllData=async(req,res)=>{
-let data=await     BatchCompletedModel.find().populate("CourseId").populate("Astudent")
-res.send({data})
+const getAllData = async (req, res) => {
+  try {
+    let data = await BatchCompletedModel.find().populate("CourseId").populate("Astudent");
+    res.send({ data });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ msg: "Internal server error", error: error.message });
+  }
 }
 
-module.exports={Update,getAllData}
+module.exports = { Update, getAllData };
